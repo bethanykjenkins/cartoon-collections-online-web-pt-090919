d1fe6757bb22be4db583327453344a5d3cba68fe
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-10-01 13:48:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(names) # code an argument here
   # Your code here
   names.each_with_index do |index, name|
     index += 1 
-    puts 
+    puts "#{index} "
 end
 
 def summon_captain_planet# code an argument here
